31ba2075d8e1d90c2a25865fe55cfcc8905ec0e9
mathias <mathias.deherdt@live.com>
2020-10-06 12:06:57 +0200

    password werkt

--- script/app.js
+++ script/app.js
@@ -1,6 +1,35 @@
-function handleFloatingLabel() {}
+function handleFloatingLabel() {
+  console.log('Ik kom in HandleFloatingLabel');
+}
 
-function handlePasswordSwitcher() {}
+const handlePasswordSwitcher = function () {
+  const passwordInput = document.querySelector('.js-password-input');
+  const passwordToggle = document.querySelector('.js-password-toggle');
+
+  console.log('Ik zit in handlePasswordSwitcher');
+
+  // kan ook op deze manier met een komma
+  // const passwordInput = document.querySelector('.js-password-input'),
+  // passwordToggle = document.querySelector('.js-password-toggle');
+
+  // simpele controle
+  if (!passwordInput || !passwordToggle) {
+    console.error('De klasses werden niet gevonden', passwordInput, passwordToggle);
+    return;
+  }
+
+  // kijk of er geklikt wordt op de checkbox
+  passwordToggle.addEventListener('change', function () {
+    // als er geklikt wordt, veranderen we het type van input password naar text
+    if (passwordInput.type == 'password') {
+      console.log('text');
+      passwordInput.type = 'text';
+    } else {
+      console.log('password');
+      passwordInput.type = 'password';
+    }
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Script loaded!');
